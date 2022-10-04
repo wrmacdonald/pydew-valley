f280456bfa2071bbf8dba4b8978ca66057efdd66
wrmacdonald <wrmacdonald2016@gmail.com>
2022-10-04 15:21:08 -0700

    2:42:45 Tutorial - Collisions

--- code/level.py
+++ code/level.py
@@ -15,6 +15,7 @@ class Level:
 
         # sprite groups
         self.all_sprites = CameraGroup()
+        self.collision_sprites = pygame.sprite.Group()          # sprites that can be collided with
 
         self.setup()
         self.overlay = Overlay(self.player)
@@ -33,7 +34,7 @@ class Level:
 
         # fence
         for x, y, surf in tmx_data.get_layer_by_name('Fence').tiles():
-            Generic((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+            Generic((x * TILE_SIZE, y * TILE_SIZE), surf, [self.all_sprites, self.collision_sprites])
 
         # water
         water_frames = import_folder('../graphics/water')
@@ -42,13 +43,21 @@ class Level:
 
         # trees
         for obj in tmx_data.get_layer_by_name('Trees'):
-            Tree((obj.x, obj.y), obj.image, self.all_sprites, obj.name)
+            Tree((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites], obj.name)
 
         # wildflowers
         for obj in tmx_data.get_layer_by_name('Decoration'):
-            Wildflower((obj.x, obj.y), obj.image, self.all_sprites)
+            Wildflower((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites])
 
-        self.player = Player((640, 360), self.all_sprites)
+        # collision tiles
+        for x, y, surf in tmx_data.get_layer_by_name('Collision').tiles():
+            # only exists in collision sprites, not all_sprites - won't be visible/move/update
+            Generic((x * TILE_SIZE, y * TILE_SIZE), pygame.Surface((TILE_SIZE, TILE_SIZE)), self.collision_sprites)
+
+        # player
+        for obj in tmx_data.get_layer_by_name('Player'):
+            if obj.name == 'Start':
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
         Generic(
             pos=(0, 0),
             surf=pygame.image.load('../graphics/world/ground.png').convert_alpha(),

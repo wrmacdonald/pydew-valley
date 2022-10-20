640d1c84ffe9a73ddc8397410b0317eabfc30eef
wrmacdonald <wrmacdonald2016@gmail.com>
2022-10-20 16:26:33 -0700

    6:25:10 Tutorial - The Merchant & Inventory Menu + some final fixes

--- code/player.py
+++ code/player.py
@@ -5,7 +5,7 @@ from timer import Timer
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, group, collision_sprites, tree_sprites, interaction, soil_layer):
+    def __init__(self, pos, group, collision_sprites, tree_sprites, interaction, soil_layer, toggle_shop):
         super().__init__(group)
 
         # importing animations
@@ -52,12 +52,18 @@ class Player(pygame.sprite.Sprite):
             'corn':   0,
             'tomato': 0
         }
+        self.seed_inventory = {
+            'corn': 5,
+            'tomato': 5
+        }
+        self.money = 200
 
         # interaction
         self.tree_sprites = tree_sprites
         self.interaction = interaction
         self.sleep = False
         self.soil_layer = soil_layer
+        self.toggle_shop = toggle_shop
 
     def use_tool(self):
         if self.selected_tool == 'hoe':
@@ -75,7 +81,9 @@ class Player(pygame.sprite.Sprite):
         self.target_pos = self.rect.center + PLAYER_TOOL_OFFSET[self.status.split('_')[0]]
 
     def use_seed(self):
-        self.soil_layer.plant_seed(self.target_pos, self.selected_seed)
+        if self.seed_inventory[self.selected_seed] > 0:
+            self.soil_layer.plant_seed(self.target_pos, self.selected_seed)
+            self.seed_inventory[self.selected_seed] -= 1
 
     def import_assets(self):
         # dict to get player states & animations
@@ -147,10 +155,12 @@ class Player(pygame.sprite.Sprite):
 
             # try to sleep (check near bed)
             if keys[pygame.K_RETURN]:
+                # Debug: show shop
+                self.toggle_shop()
                 collided_interaction_sprite = pygame.sprite.spritecollide(self, self.interaction, False)
                 if collided_interaction_sprite:
                     if collided_interaction_sprite[0].name == 'Trader':
-                        pass
+                        self.toggle_shop()
                     else:   # bed
                         self.status = 'left_idle'
                         self.sleep = True

1e5e7bd1d89b1ef409508acd41391d92fb9c6131
wrmacdonald <wrmacdonald2016@gmail.com>
2022-10-20 11:39:09 -0700

    5:33:00 Tutorial - Harvesting - My Bug Fix: plant images not showing in correct growth order -> sorted the image_files in support so it loops through in alphabetical order

--- code/support.py
+++ code/support.py
@@ -7,7 +7,7 @@ def import_folder(path):
 
     # walk through folders & subfolders to get list of filenames
     for folder_name, sub_folder, image_files in walk(path):
-        for image in image_files:
+        for image in sorted(image_files):
             full_path = path + '/' + image
             image_surf = pygame.image.load(full_path).convert_alpha()
             surface_list.append(image_surf)
